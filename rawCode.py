30356fd5446f3f9c6eef55eac0110f65c4ac3231
MrMimic <emeric.dynomant@gmail.com>
2017-05-17 00:56:38 +0200

    Add files via upload

---
+++ rawCode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+''' IMPORTS '''
+from urllib import request
+from bs4 import BeautifulSoup
+
+''' scraping function '''
+def scraping(URL):
+	pageResponse = request.urlopen(URL)
+	pageData = pageResponse.read()
+	return pageData
+	
+def parsing(pageData):
+	pageSoup = BeautifulSoup(pageData, 'html.parser')
+	rawRecette = pageSoup.find_all('div', attrs = {'class': 'm_content_recette_todo'})
+	recette = str(rawRecette[0]).split('<br/>')
+	for etape in recette:
+		if etape != '':
+			print('- {}'.format(etape))
+	
+pageWeb = scraping('http://www.marmiton.org/recettes/recette_pates-a-la-carbonara_80453.aspx')
+recette = parsing(pageWeb)
