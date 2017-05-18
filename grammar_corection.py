4b7d040e5d63f4e979d850c016a429ef2f316dde
MrMimic <emeric.dynomant@gmail.com>
2017-05-18 19:12:03 +0200

    Create grammar_corection.py

---
+++ grammar_corection.py
@@ -0,0 +1,37 @@
+
+import re
+import time
+import json
+import requests
+from urllib import parse
+start = time.time()
+
+textToCorrect = 'telephonne appele apelle appelle'
+
+''' HEADER FOR API CONNECTIONS '''
+spellCheck = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Ocp-Apim-Subscription-Key': '80349415134845a296b74ed62a1cf9a0' }
+''' SPELL CHECK PARAMETERS '''
+params = parse.urlencode({
+'mode': 'spell',
+'mkt': 'fr-fr',
+'text': textToCorrect 
+})
+
+''' SPELL CHECK REQUEST '''
+rawData = requests.get("https://api.cognitive.microsoft.com/bing/v5.0/SpellCheck/?%s" % params, headers=spellCheck)
+jsonData = json.loads(rawData.text)
+
+''' IF MISTAKE, PRINT '''
+if len(jsonData['flaggedTokens']) > 0:
+	print('Analysed in {} sec'.format(round(time.time() - start, 3)))
+	print('- ' * 50)
+	for mistake in jsonData['flaggedTokens']:
+		print('Written: {}\t\tCorrect: {}\t\t(score: {})'.format(mistake['token'], mistake['suggestions'][0]['suggestion'], round(mistake['suggestions'][0]['score'], 3)))
+		if mistake['suggestions'][0]['score'] > 0.8:
+			textToCorrect = re.sub(mistake['token'], mistake['suggestions'][0]['suggestion'], textToCorrect)
+	print('- ' * 50)
+	print('Corrected output :\n{}'.format(textToCorrect))
+else:
+	print('Good boy, everything\'s correct')
